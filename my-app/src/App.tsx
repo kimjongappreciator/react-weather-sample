41a38e08e6f3b7b7b68edf7dd891f382272d0b8a
Sebastian Chacon <nonexistentexistentialist1@gmail.com>
2025-07-14 15:57:09 -0500

    update

--- my-app/src/App.tsx
+++ my-app/src/App.tsx
@@ -1,16 +1,10 @@
 //import "./App.css";
-
-import { ThemeProvider } from "./components/theme-provider";
-import { ModeToggle } from "./components/ui/mode-toggle";
 import Home from "./components/views/home";
 
 function App() {
   return (
     <>
-      <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
-        <ModeToggle />
-        <Home></Home>
-      </ThemeProvider>
+      <Home></Home>
     </>
   );
 }
